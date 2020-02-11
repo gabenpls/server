9fc930e353edbde67d49f698a429585c1a76e64d
McSim <McSim1993@gmail.com>
2020-02-11 23:57:12 +0300

    and another one

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 set -e
 
 sbt debian:packageBin
-rsync target/server_1.0-SNAPSHOT_all.deb root@167.172.101.7:~/ -v -r --update
+scp target/server_1.0-SNAPSHOT_all.deb root@167.172.101.7:~/server_1.0-SNAPSHOT_all.deb
 ssh root@167.172.101.7 "dpkg -i server_1.0-SNAPSHOT_all.deb && service server restart"
